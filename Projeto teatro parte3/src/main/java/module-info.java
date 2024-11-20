60c49bc7dfd478e34c724e88283b0eb7605bb96c
Heitor Ferreira <56131014+Ghoscop@users.noreply.github.com>
2024-11-20 12:42:25 -0300

    correção do bug dos botões

---
+++ Projeto teatro parte3/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module heitor.larysson.matheus.natan.projeto_teatro_parte1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens Teatro_projeto_teatro_parte1 to javafx.fxml;
+    exports Teatro_projeto_teatro_parte1;
+    exports Teatro_projeto_teatro_parte1.Controller;
+    opens Teatro_projeto_teatro_parte1.Controller to javafx.fxml;
+    exports Teatro_projeto_teatro_parte1.Interface;
+    opens Teatro_projeto_teatro_parte1.Interface to javafx.fxml;
+}
