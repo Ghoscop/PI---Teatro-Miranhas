93d9609c27f6b6e6bea1cf6636ae927275192e6d
Heitor Ferreira <56131014+Ghoscop@users.noreply.github.com>
2024-11-20 12:43:00 -0300

    Feito a parte de cadastro
    atualizada

---
+++ Projeto teatro parte4/src/main/java/Teatro_projeto_teatro_parte1/Main.java
@@ -0,0 +1,10 @@
+package Teatro_projeto_teatro_parte1;
+
+import Teatro_projeto_teatro_parte1.Interface.Interface;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Interface.class, args);
+    }
+}
