a33aea615a7ca2b73b9271a94727a80ab45c2f26
Heitor Ferreira <56131014+Ghoscop@users.noreply.github.com>
2024-11-20 12:41:21 -0300

    Feito o botão para outra cena de cadastro
    Erro ao trocar de cena os botões minimizar e fechar n funcionam

---
+++ Projeto teatro parte2/src/main/java/Teatro_projeto_teatro_parte1/Controller/Controller.java
@@ -0,0 +1,14 @@
+package Teatro_projeto_teatro_parte1.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
